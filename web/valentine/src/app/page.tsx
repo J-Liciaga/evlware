6dafda994d4380a2a5f3c9b370ef57f9a88ba54c
J-Liciaga <josiah.liciagasilva@protonmail.com>
2025-01-12 20:36:01 -0500

    feat(workspace): initial workspace configuration

--- web/valentine/src/app/page.tsx
+++ web/valentine/src/app/page.tsx
@@ -11,7 +11,7 @@ export default function Index() {
           <div id="welcome">
             <h1>
               <span> Hello there, </span>
-              Welcome marketing 👋
+              Welcome valentine 👋
             </h1>
           </div>
 
@@ -395,7 +395,7 @@ export default function Index() {
                 </svg>
                 View project details
               </summary>
-              <pre>nx show project marketing --web</pre>
+              <pre>nx show project valentine --web</pre>
             </details>
             <details>
               <summary>
